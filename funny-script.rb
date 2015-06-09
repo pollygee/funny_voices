382095bb9cf1856259109e6e8131af2ea1adefe0
Bonnie Allen <Allen@King-Kittenheads-MacBook-Pro.local>
2015-06-09 17:22:47 -0400

    Update Script

--- funny-script.rb
+++ funny-script.rb
@@ -1,10 +1,17 @@
-require 'funnytalk-api'
+require './funnytalk-api'
 require 'pry'
 
 f = FunnyTalkAPI.new
-t = "I'm a boss!"
-v = Amelie
+t = "I'm a bawse! I am the best."
+v = "Amelie"
 
 f.say_something t, v
+
+f.say_something "The project is not so hard after all", "Bad News"
+
+f.say_something "Today you are You, that is truer than true. There is no one alive who is Youer than You", "Ralph"
+
+
+
 
 
